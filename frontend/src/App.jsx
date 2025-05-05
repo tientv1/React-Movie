3a7fecbd20455a367c25676352e0cd372af1492c
tientv <tientranvan19052004@gmail.com>
2025-05-06 03:23:35 +0700

    Create

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
 import Home from "./pages/Home";
+import Favorite from "./pages/Favorites";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
     return (
-        <>
-            <Home />
-        </>
+        <main className="main-content">
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/favorites" element={<Favorite />} />
+            </Routes>
+        </main>
     );
 }
 
