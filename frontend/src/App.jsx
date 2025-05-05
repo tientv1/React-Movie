045dc5ce9fe02310f47c7b28950e92ffaddcf1bd
tientv <tientranvan19052004@gmail.com>
2025-05-06 03:35:29 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,19 @@ import "./App.css";
 import Home from "./pages/Home";
 import Favorite from "./pages/Favorites";
 import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
     return (
-        <main className="main-content">
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/favorites" element={<Favorite />} />
-            </Routes>
-        </main>
+        <div>
+            <Navbar />
+            <main className="main-content">
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/favorites" element={<Favorite />} />
+                </Routes>
+            </main>
+        </div>
     );
 }
 
