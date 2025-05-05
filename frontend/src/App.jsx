9c42daee63725a47ecdeada63b1dc292001ae92f
tientv <tientranvan19052004@gmail.com>
2025-05-06 03:08:44 +0700

    Create

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,11 @@ import "./App.css";
 import Home from "./pages/Home";
 
 function App() {
-    return <></>;
+    return (
+        <>
+            <Home />
+        </>
+    );
 }
 
 export default App;
