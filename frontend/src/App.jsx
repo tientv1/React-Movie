16e8750125258e2633987baf855f5ee9a94b9ff0
tientv <tientranvan19052004@gmail.com>
2025-05-06 00:54:30 +0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,7 @@
 import "./App.css";
 
 function App() {
-    return (
-        <>
-            <Text display={"What's Up?"} />
-            <Text display={"Hello"} />
-        </>
-    );
-}
-
-function Text({ display }) {
-    return (
-        <div>
-            <p>{display}</p>
-        </div>
-    );
+    return <></>;
 }
 
 export default App;
