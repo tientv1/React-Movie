16a4004e6cebe750dd6a5c9d5a0209eb67718461
tientv <tientranvan19052004@gmail.com>
2025-05-06 01:35:21 +0700

    Create MovieCard.jsx

---
+++ frontend/src/components/MovieCard.jsx
@@ -0,0 +1,37 @@
+function MovieCard({ movie }) {
+    function onFavoriteClick() {
+        alert("Clicked");
+    }
+
+    return (
+        <div className="movie-card">
+            <div className="movie-poster">
+                <img src={movie.url} alt={movie.title} />
+                <div className="movie-overlay">
+                    <button className="favorite-btn" onClick={onFavoriteClick}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="w-6 h-6"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                            />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+            <div className="movie-info">
+                <h3>{movie.title}</h3>
+                <p>{movie.release_date}</p>
+            </div>
+        </div>
+    );
+}
+
+export default MovieCard;
