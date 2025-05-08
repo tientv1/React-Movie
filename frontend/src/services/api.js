90073286e67475dcab6fec0919a45374ad949c2b
tientv <tientranvan19052004@gmail.com>
2025-05-08 13:11:23 +0700

    Create api.js

---
+++ frontend/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "35b46a0f661bdce1991d0922363baf4f";
+const BASE_URL = "https://api.themoviedb.org/3";
+export const getPopularMovies = async () => {
+    const response = await fetch(
+        `${BASE_URL}/movie/popular?api_key=${API_KEY}`
+    );
+    const data = response.json();
+    return data.results;
+};
+export const searchMovies = async (query) => {
+    const response = await fetch(
+        `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+            query
+        )}`
+    );
+    const data = response.json();
+    return data.results;
+};
